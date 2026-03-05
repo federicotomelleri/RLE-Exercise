cd6d9749d4cfcfc238029bd2290968c1bccef234
federicotomelleri <code.cooperation@mm.st>
2026-03-06 00:54:13 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public String encode(String s) {
+        // write your code here
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
